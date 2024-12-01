0975555b75fdedb8f30c8f34d8fe58b9db776731
mhafez1978 <mhafez1978@outlook.com>
2024-12-01 17:22:03 -0500

    modified another api route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,10 +1,30 @@
-export const revalidate = 60;
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const data = await fetch(
-    "https://api.blooming-brands.com/wp-json/wp/v2/users/"
-  );
-  const users = await data.json();
+  try {
+    const response = await fetch(
+      "https://api.blooming-brands.com/wp-json/wp/v2/users"
+    );
 
-  return Response.json(users);
+    if (!response.ok) {
+      return NextResponse.json(
+        { error: `Failed to fetch posts: ${response.statusText}` },
+        { status: response.status }
+      );
+    }
+
+    const users = await response.json();
+    //console.log(posts);
+
+    return NextResponse.json(users);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    } else {
+      return NextResponse.json(
+        { error: "An unknown error occurred" },
+        { status: 500 }
+      );
+    }
+  }
 }
