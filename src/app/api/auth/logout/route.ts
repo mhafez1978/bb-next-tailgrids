a920148771709c31319f396b221708997db1bfff
mhafez1978 <mhafez1978@outlook.com>
2024-12-09 04:32:12 -0500

    fixed 3

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,25 +1,7 @@
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   const response = NextResponse.redirect("/login"); // Redirect to login page
-
-//   // Clear the token cookie
-//   response.cookies.set("token", "", {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === "production",
-//     path: "/",
-//     maxAge: 0, // Expire immediately
-//   });
-
-//   return response;
-// }
-
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const response = NextResponse.redirect(
-    `${process.env.NEXT_PUBLIC_BASE_URL_DEV}/login`
-  ); // Use absolute URL
+  const response = NextResponse.redirect("https://blooming-brands.com/login"); // Redirect to login page
 
   // Clear the token cookie
   response.cookies.set("token", "", {
@@ -31,3 +13,20 @@ export async function GET() {
 
   return response;
 }
+
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   const response = NextResponse.redirect(
+//     `${process.env.NEXT_PUBLIC_BASE_URL_DEV}/login`
+//   ); // Use absolute URL
+
+//   // Clear the token cookie
+//   response.cookies.set("token", "", {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === "production",
+//     path: "/",
+//     maxAge: 0, // Expire immediately
+//   });
+// return response;
+//}
